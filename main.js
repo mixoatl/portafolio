8d6c548c0ecfad395b1724421217da4b39f2b2b5
Monserrat Fernández <xiomistar2003@hotmail.com>
2024-04-23 23:39:49 -0600

    Actualización al portafolio

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-// Área del círculo => es PI por radioCirculoAlCuadrado
-
-const PI = 3.1416;
- 
-let areaCirculo;
-
-let radio = 4;
-
-areaCirculo = PI * (radio * radio);
-//document.write(areaCirculo);
-
-radio = 10;
-
-areaCirculo = PI * (radio * radio);
-//document es el documento HTML al que está ligado el archivo
-// el operador . me permite acceder a propiedades o métodos(acciones) del documento
-//write es un método (función que le pertenece a alguien)
-//document.write(areaCirculo);
-
-/* const myModal = document.getElementById('modal-contactame');
-const myElement = document.getElementById('contactame');
-
-myModal.addEventListener('shown.bs.modal', () => {
-    myElement.focus();
-}); */
-
-
 let app = document.getElementById('typewriter');
 
 let typewriter = new Typewriter(app, {
@@ -35,7 +8,7 @@ let typewriter = new Typewriter(app, {
 typewriter
 //1000 milisegundos es 1 segundo
   .pauseFor(1000)
-  .typeString('Desarrolladora Web')
+  .typeString('Diseñadora Web')
   .deleteChars(20)
   .typeString('Monserrat Fernández')
   .pauseFor(300)
